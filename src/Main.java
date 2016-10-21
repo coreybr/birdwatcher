232c2f10b6b6a53d28e9c54457e0184ff67c9788
Corey Bradley <coreytbradley@gmail.com>
2016-10-20 21:24:29 -0400

    Factor out UI class
    Fixes #2

--- src/Main.java
+++ src/Main.java
@@ -3,64 +3,9 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        Database db = new Database();
-        String input;
-
-        //Print help initially
-        printHelp();
-        
-        //Main input loop
-        while (true) {
-            System.out.print("? ");
-            input = reader.nextLine();
-            //Make input case-insensitive
-            input = input.trim().toUpperCase();
-
-            if (input.equals("ADD")) {
-                System.out.print("Name: ");
-                String name = reader.nextLine();
-                System.out.print("Latin Name: ");
-                String latinName = reader.nextLine();
-                db.addBird(new Bird(name, latinName));
-
-            } else if (input.equals("OBSERVE")) {
-                System.out.print("What was observed:? ");
-                String name = reader.nextLine();
-                if (db.hasByName(name)) {
-                    db.observeByName(name);
-                } else {
-                    System.out.println("Sorry, I don't recognize that bird.");
-                }
-
-            } else if (input.equals("SHOW")) {
-                System.out.print("Which bird to show? ");
-                input = reader.nextLine();
-                System.out.println(db.showByName(input));
-
-            } else if (input.equals("STATISTICS")) {
-                db.printBirds();
-
-            } else if (input.equals("HELP") || input.equals("?")) {
-                printHelp();
-
-            } else if (input.equals("QUIT")) {
-                break;
-            } else {
-                System.out.println("Sorry, that's not a recognized command.");
-            }
-
-        }
-        reader.close();
-    }
-    
-    public static void printHelp(){
-    	System.out.println("Add - adds a bird");
-        System.out.println("Observe - adds an observation");
-        System.out.println("Statistics - prints all birds");
-        System.out.println("Show - prints specified bird");
-        System.out.println("Quit - terminates the program");
-    }
+	public static void main(String[] args) {
+		UI ui = new UI();
+		ui.start();
+	}
 
 }
---
+++ src/UI.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class UI {
+
+	private Scanner reader;
+	private Database db;
+	private String input;
+
+	public UI() {
+		reader = new Scanner(System.in);
+		db = new Database();
+	}
+
+	public void start() {
+		// Print help initially
+		printHelp();
+
+		// Main input loop
+		while (true) {
+			System.out.print("? ");
+			input = reader.nextLine();
+			// Make input case-insensitive
+			input = input.trim().toUpperCase();
+
+			if (input.equals("ADD")) {
+				System.out.print("Name: ");
+				String name = reader.nextLine();
+				System.out.print("Latin Name: ");
+				String latinName = reader.nextLine();
+				db.addBird(new Bird(name, latinName));
+
+			} else if (input.equals("OBSERVE")) {
+				System.out.print("What was observed:? ");
+				String name = reader.nextLine();
+				if (db.hasByName(name)) {
+					db.observeByName(name);
+				} else {
+					System.out.println("Sorry, I don't recognize that bird.");
+				}
+
+			} else if (input.equals("SHOW")) {
+				System.out.print("Which bird to show? ");
+				input = reader.nextLine();
+				System.out.println(db.showByName(input));
+
+			} else if (input.equals("STATISTICS")) {
+				db.printBirds();
+
+			} else if (input.equals("HELP") || input.equals("?")) {
+				printHelp();
+
+			} else if (input.equals("QUIT")) {
+				break;
+			} else {
+				System.out.println("Sorry, that's not a recognized command.");
+			}
+
+		}
+		reader.close();
+	}
+
+	public static void printHelp() {
+		System.out.println("Add - adds a bird");
+		System.out.println("Observe - adds an observation");
+		System.out.println("Statistics - prints all birds");
+		System.out.println("Show - prints specified bird");
+		System.out.println("Quit - terminates the program");
+	}
+
+}
