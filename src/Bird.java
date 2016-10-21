38741e34188db9a93b8e5620ad89227b80e8c007
Corey Bradley <coreytbradley@gmail.com>
2016-10-20 21:23:39 -0400

    Perform Eclipse auto formatting

--- src/Bird.java
+++ src/Bird.java
@@ -1,33 +1,33 @@
 import java.io.Serializable;
 
-public class Bird  implements Serializable {
+public class Bird implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private String name;
-    private String latinName;
-    private int observations;
+	private String latinName;
+	private int observations;
 
-    public Bird(String name, String latinName) {
-        this.name = name;
-        this.latinName = latinName;
-        this.observations = 0;
-    }
+	public Bird(String name, String latinName) {
+		this.name = name;
+		this.latinName = latinName;
+		this.observations = 0;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void observe() {
-        this.observations++;
-    }
+	public void observe() {
+		this.observations++;
+	}
 
-    @Override
-    public String toString() {
-        if (observations == 1) {
-            return this.name + " (" + latinName + "): " + this.observations + " observation";
-        } else {
-            return this.name + " (" + latinName + "): " + this.observations + " observations";
-        }
-    }
+	@Override
+	public String toString() {
+		if (observations == 1) {
+			return this.name + " (" + latinName + "): " + this.observations + " observation";
+		} else {
+			return this.name + " (" + latinName + "): " + this.observations + " observations";
+		}
+	}
 
 }
--- src/Database.java
+++ src/Database.java
@@ -9,99 +9,97 @@ import java.util.ArrayList;
 
 public class Database {
 
-    private ArrayList<Bird> birds;
-    private String filePath = "birds.ser";
+	private ArrayList<Bird> birds;
+	private String filePath = "birds.ser";
 
-    public Database() {
-    	loadBirds();
-    }
+	public Database() {
+		loadBirds();
+	}
 
-    /*
-     * Load List of birds from file.
-     * Create new list if file not found.
-     */
-    private void loadBirds(){
-    	File f = new File(filePath);
-    	if(f.exists() && !f.isDirectory()) { 
-    		try {
-                FileInputStream fis = new FileInputStream(filePath);
-                ObjectInputStream in = new ObjectInputStream(fis);
-                this.birds = (ArrayList<Bird>)in.readObject();
-                in.close();
-            }
-            catch (Exception e) {
-                System.out.println(e);
-            }
-    	} else {
-    		birds = new ArrayList<Bird>();
-    	}    
-    }
-    
-    /*
-     * Persist list of birds to file.
-     */
-    private void saveBirds(){
-        ObjectOutputStream oos = null;
-        FileOutputStream fout = null;
-        try{
-            fout = new FileOutputStream(filePath, false);
-            oos = new ObjectOutputStream(fout);
-            oos.writeObject(birds);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }finally {
-            if(oos  != null){
-                try {
+	/*
+	 * Load List of birds from file. Create new list if file not found.
+	 */
+	private void loadBirds() {
+		File f = new File(filePath);
+		if (f.exists() && !f.isDirectory()) {
+			try {
+				FileInputStream fis = new FileInputStream(filePath);
+				ObjectInputStream in = new ObjectInputStream(fis);
+				this.birds = (ArrayList<Bird>) in.readObject();
+				in.close();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		} else {
+			birds = new ArrayList<Bird>();
+		}
+	}
+
+	/*
+	 * Persist list of birds to file.
+	 */
+	private void saveBirds() {
+		ObjectOutputStream oos = null;
+		FileOutputStream fout = null;
+		try {
+			fout = new FileOutputStream(filePath, false);
+			oos = new ObjectOutputStream(fout);
+			oos.writeObject(birds);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (oos != null) {
+				try {
 					oos.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-            } 
-        }
-    }
-    
-    public void addBird(Bird newBird) {
-        birds.add(newBird);
-        saveBirds();
-    }
+			}
+		}
+	}
 
-    public void printBirds() {
-        for (Bird bird : birds) {
-            System.out.println(bird);
-        }
-        if (birds.isEmpty()){
-        	System.out.println("No birds recorded.");
-        }
-    }
+	public void addBird(Bird newBird) {
+		birds.add(newBird);
+		saveBirds();
+	}
 
-    public boolean hasByName(String name) {
-        ArrayList<Bird> result = new ArrayList<Bird>();
-        for (Bird bird : birds) {
-            if (StringUtils.included(bird.getName(), name)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public void printBirds() {
+		for (Bird bird : birds) {
+			System.out.println(bird);
+		}
+		if (birds.isEmpty()) {
+			System.out.println("No birds recorded.");
+		}
+	}
 
-    public void observeByName(String name) {
-        ArrayList<Bird> result = new ArrayList<Bird>();
-        for (Bird bird : birds) {
-            if (StringUtils.included(bird.getName(), name)) {
-                bird.observe();
-                saveBirds();
-            }
-        }
-    }
+	public boolean hasByName(String name) {
+		ArrayList<Bird> result = new ArrayList<Bird>();
+		for (Bird bird : birds) {
+			if (StringUtils.included(bird.getName(), name)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public String showByName(String name) {
-        ArrayList<Bird> result = new ArrayList<Bird>();
-        for (Bird bird : birds) {
-            if (StringUtils.included(bird.getName(), name)) {
-                return bird.toString();
-            }
-        }
-        return "Is not a bird!";
-    }
+	public void observeByName(String name) {
+		ArrayList<Bird> result = new ArrayList<Bird>();
+		for (Bird bird : birds) {
+			if (StringUtils.included(bird.getName(), name)) {
+				bird.observe();
+				saveBirds();
+			}
+		}
+	}
+
+	public String showByName(String name) {
+		ArrayList<Bird> result = new ArrayList<Bird>();
+		for (Bird bird : birds) {
+			if (StringUtils.included(bird.getName(), name)) {
+				return bird.toString();
+			}
+		}
+		return "Is not a bird!";
+	}
 
 }
